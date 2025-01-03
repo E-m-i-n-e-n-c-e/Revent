a33ba283bb5b081fec1da4fdeea5215830c3ad7e
E-m-i-n-e-n-c-e <kssakhilraj@gmail.com>
2025-01-03 12:22:37 +0530

    Added navbar

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:events_manager/screens/dashboard/widgets/announcements_slider.dart';
+import 'package:events_manager/screens/dashboard/widgets/bottom_navbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -28,6 +29,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const CustomBottomNavigationBar(),
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
--- lib/screens/dashboard/widgets/bottom_navbar.dart
+++ lib/screens/dashboard/widgets/bottom_navbar.dart
@@ -1,62 +1,80 @@
 import 'package:flutter/material.dart';
 
-class CustomBottomNavigationBar extends StatelessWidget {
+class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({super.key});
 
   @override
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(maxWidth: 357),
+    return SafeArea(
       child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xFF203A43),
-              Color(0xFF0F2027),
-            ],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-          ),
-          borderRadius: BorderRadius.circular(35),
-        ),
-        padding: const EdgeInsets.symmetric(horizontal: 31, vertical: 7),
+        constraints: const BoxConstraints(maxWidth: 350),
+        color: const Color(0xFF04161D),
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 8),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            IconButton(
-              icon: const Icon(Icons.calendar_month_outlined),
-              color: Colors.white,
-              iconSize: 43,
-              onPressed: () {
-                // Handle navigation to home
-              },
+            _buildNavItem(
+              icon: Icons.home,
+              index: 0,
             ),
-            IconButton(
-              icon: const Icon(Icons.home),
-              color: Colors.white,
-              iconSize: 43,
-              onPressed: () {
-                // Handle navigation to search
-              },
+            _buildNavItem(
+              icon: Icons.calendar_month_outlined,
+              index: 1,
             ),
-            IconButton(
-              icon: const Icon(Icons.auto_stories),
-              color: Colors.white,
-              iconSize: 43,
-              onPressed: () {
-                // Handle navigation to notifications
-              },
+            _buildNavItem(
+              icon: Icons.search,
+              index: 2,
             ),
-            IconButton(
-              icon: const Icon(Icons.chat),
-              color: Colors.white,
-              iconSize: 43,
-              onPressed: () {
-                // Handle navigation to profile
-              },
+            _buildNavItem(
+              icon: Icons.auto_stories,
+              index: 3,
+            ),
+            _buildNavItem(
+              icon: Icons.chat,
+              index: 4,
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem({required IconData icon, required int index}) {
+    bool isSelected = _selectedIndex == index;
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        padding: const EdgeInsets.all(0),
+      ),
+      onPressed: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            color: const Color(0xff71C2E4),
+            size: 26,
+          ),
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            height: 2,
+            width: isSelected ? 20 : 0,
+            color: isSelected ? const Color(0xff71C2E4) : Colors.transparent,
+          ),
+        ],
       ),
     );
   }
